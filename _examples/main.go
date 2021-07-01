1ffa7c41b7557da628b3ffb42f817166dea317c6
lixiang4u <lixiang4u@gmail.com>
2021-07-01 11:11:48 +0800

    修改初始化方式，websocket只注册一次事件

--- _examples/main.go
+++ _examples/main.go
@@ -14,28 +14,24 @@ var (
 )
 
 func main() {
+	var ws = go_websocket.WSWrapper{}
+	ws.Config.Debug = true
+	//注册列表数据查询
+	ws.On("list", func(clientId string, ws *websocket.Conn, messageType int, data map[string]interface{}) bool {
+		b, _ := json.MarshalIndent(go_websocket.WSConnectionList(), "", "	")
+		_ = ws.WriteMessage(messageType, b)
+		return true
+	})
+	//注册广播消息
+	ws.On("broadcast", func(clientId string, ws *websocket.Conn, messageType int, data map[string]interface{}) bool {
+		b, _ := json.MarshalIndent(data, "", "	")
+		go_websocket.WSBroadcast(clientId, messageType, b)
+		return true
+	})
 
-	http.Handle("/", http.FileServer(http.Dir("./")))
+	http.Handle("/", http.FileServer(http.Dir("./_examples")))
 	http.HandleFunc("/register", func(writer http.ResponseWriter, request *http.Request) {
-
-		var ws = go_websocket.WSWrapper{}
-		ws.Config.Debug = true
-		//注册列表数据查询
-		ws.On("list", func(clientId string, ws *websocket.Conn, messageType int, data map[string]interface{}) bool {
-			b, _ := json.MarshalIndent(go_websocket.WSConnectionList(), "", "	")
-			_ = ws.WriteMessage(messageType, b)
-			return true
-		})
-		//注册广播消息
-		ws.On("broadcast", func(clientId string, ws *websocket.Conn, messageType int, data map[string]interface{}) bool {
-			b, _ := json.MarshalIndent(data, "", "	")
-			go_websocket.WSBroadcast(clientId, messageType, b)
-			return true
-		})
-
-		// 运行
 		ws.Run(writer, request, nil)
-
 	})
 
 	fmt.Printf("Now listening on: http://%s\r\n", addr)
