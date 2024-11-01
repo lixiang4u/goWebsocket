af037ed01628fe7d3d33990e760bbd94255195a0
example <dev@example.com>
2024-11-01 22:20:23 +0800

    修改消息数据结构

--- handler.go
+++ handler.go
@@ -154,12 +154,13 @@ func (x *WebsocketManager) eventSendToUidHandler(clientId string, ws *websocket.
 func (x *WebsocketManager) eventSendToGroupHandler(clientId string, ws *websocket.Conn, messageType int, data EventProtocol) bool {
 	x.Log("[eventSendToGroupHandler] %s", clientId)
 
-	var clients []string
-	switch data.Data.(type) {
-	case string:
-		clients = x.Conn.GetGroupClientIds(data.Data.(string))
-	default:
+	if len(data.Group) == 0 {
+		x.Log("[SendToGroup] missing param(group)")
 		return false
+	}
+	var clients = x.Conn.GetGroupClientIds(data.Group)
+	if len(clients) == 0 {
+		return true
 	}
 	var transferBuffer = x.ToBytes(data)
 	for _, tmpClientId := range clients {
@@ -182,25 +183,23 @@ func (x *WebsocketManager) eventBroadcastHandler(clientId string, ws *websocket.
 func (x *WebsocketManager) eventJoinGroupHandler(clientId string, ws *websocket.Conn, messageType int, data EventProtocol) bool {
 	x.Log("[eventJoinGroupHandler] %s", clientId)
 
-	switch data.Data.(type) {
-	case string:
-		x.Conn.JoinGroup(clientId, data.Data.(string))
-		return true
-	default:
+	if len(data.Group) == 0 {
+		x.Log("[SendToGroup] missing param(group)")
 		return false
 	}
+	x.Conn.JoinGroup(clientId, data.Group)
+	return true
 }
 
 func (x *WebsocketManager) eventLeaveGroupHandler(clientId string, ws *websocket.Conn, messageType int, data EventProtocol) bool {
 	x.Log("[eventLeaveGroupHandler] %s", clientId)
 
-	switch data.Data.(type) {
-	case string:
-		x.Conn.LeaveGroup(clientId, data.Data.(string))
-		return true
-	default:
+	if len(data.Group) == 0 {
+		x.Log("[SendToGroup] missing param(group)")
 		return false
 	}
+	x.Conn.LeaveGroup(clientId, data.Group)
+	return true
 }
 
 func (x *WebsocketManager) eventListGroupHandler(clientId string, ws *websocket.Conn, messageType int, data EventProtocol) bool {
@@ -216,15 +215,16 @@ func (x *WebsocketManager) eventListGroupHandler(clientId string, ws *websocket.
 func (x *WebsocketManager) eventListGroupClientHandler(clientId string, ws *websocket.Conn, messageType int, data EventProtocol) bool {
 	x.Log("[eventListGroupClientHandler] %s", clientId)
 
-	switch data.Data.(type) {
-	case string:
-		x.Send(clientId, messageType, x.ToBytes(H{
-			"groups": x.Conn.ListGroupClientIds(data.Data.(string)),
-		}))
-		return true
+	if len(data.Group) == 0 {
+		x.Log("[SendToGroup] missing param(group)")
+		return false
 	}
 
+	x.Send(clientId, messageType, x.ToBytes(H{
+		"groups": x.Conn.ListGroupClientIds(data.Group),
+	}))
 	return true
+
 }
 
 // Send 对外接口，用于发送ws消息到指定clientId
--- protocol.go
+++ protocol.go
@@ -2,13 +2,14 @@ package goWebsocket
 
 // EventProtocol ws数据交互格式，基于json，event字段必选
 type EventProtocol struct {
-	ClientId string      `json:"client_id"`
+	ClientId string      `json:"client_id,omitempty"`
+	Group    string      `json:"group,omitempty"` // 调用sendToGroup时的group
 	Event    string      `json:"event"`
 	Data     interface{} `json:"data"`
 }
 
 type EventProtocolConnect struct {
-	ClientId string `json:"client_id"`
+	ClientId string `json:"client_id,omitempty"`
 	Event    string `json:"event"`
 	Data     struct {
 		ClientId string `json:"client_id"`
